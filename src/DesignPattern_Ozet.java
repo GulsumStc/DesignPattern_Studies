53eaf33481184a6d2a6ecd32777ee88e4f23b834
GulsumStc <121710851+GulsumStc@users.noreply.github.com>
2023-05-13 18:48:11 +0300

    16.05.23

--- src/DesignPattern_Ozet.java
+++ src/DesignPattern_Ozet.java
@@ -1,12 +1,55 @@
+
+
+
 /*
-    Design Pattern: kodsal problemlere sunulan en effective cozumlerdir.
 
-    Opp concept'leriyle
+ ***************************** DESİGN PATTERN *****************************
 
-    Problemi tespit edip en iyi designpatter secimi yapilir.
-
-
-
+    **Design Pattern(Tasarım Kalıpları): Benzer problemlere karşı benzer çözüm önerileri üretmek.
+    Kısaca Design Pattern, yazılım dünyasında kendimizi tekrar ede ede, karşılaştığımız problemlere
+    benzer çözümler önere önere elde ettiğimiz, best practice çözüm yolu budur dediğimiz kalıplara verilen genel isimdir.
 
 
+
+
+**Bu 23 Design Pattern 3 temel başlık altında toplanmıştır.
+Creational Design Patterns: Objelerin create edilmesiyle alakalıdır.
+-Abstract factory
+-Builder
+-Factory Method
+-Prototype
+-SingletonDP:
+
+Behavioral Design Patterns: Objelerin birbirleriyle alakası etkileşimi ile ilgili yaşanan problemlere
+çözüm önerisi getiriyor.
+-Chain of Responsibility
+-Command
+-Interpreter
+-Iterator
+-Mediator
+-Memento
+-Observer
+-State
+-Strategy
+-Template Method
+-Visitor
+
+Structural Design Patterns: Classların kendi yapılarıyla alakalı karşılaşılan problemlere çözüm önerisi
+getiriyor.Mesela bir application tasarlamamız lazım, elimizde de birden fazla class var, bunların yapısal,
+ kurgusal durumlarıyla alakalı yaşadığımız problemlere çözüm önerisi getiriyor.
+-Adapter
+-Bridge
+-Composite
+-Decorator
+-Facade
+-Flyweight
+-Proxy
+
+
+**Her bir design pattern bir tane probleme çözüm sunuyor.
+Önce problemi teşhis ediyor daha sonra bu probleme çözüm olabilecek belki yüzlerce yoldan en önemlisini en efektifini bize sunuyor.
+
+**Neden iç yapıyı bilmiyoruz?
+1-Güvenlik
+2-Bilinmesine gerek yok
  */
